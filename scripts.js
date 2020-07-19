3d845401bc759fe8974bc2cfd5256e0d29fc81e1
Lubomír Pařil <luboparil@gmail.com>
2020-07-19 09:32:47 +0200

    task completed

--- scripts.js
+++ scripts.js
@@ -4,5 +4,18 @@ names.forEach(element => {
 	if (element === "Lubomir") {
 		element = '<strong>'+ element + '</strong>';
 	}
+
 	$('ul').append('<li>'+ element +'</li>\n');
 });
+
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+};
+
+$('body').append(
+	'<div>\n'
+		+'\t<h2>'+ additionalBlock.title +'</h2>\n'
+		+'\t<p>'+ additionalBlock.text +'</p>\n'
+	+'</div>\n'
+);
